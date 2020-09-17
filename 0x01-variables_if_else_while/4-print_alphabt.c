e713a7b7597f713693eb3f8458eab85e10170975
Julian <1890@holbertonschool.com>
2020-09-17 15:19:40 +0000

    0x01C

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Return: always 0 (Success)
+ */
+int main(void)
+{
+int ch = 'a';
+
+while (ch <= 'z')
+{
+if (ch != 'e' && ch != 'q')
+putchar(ch);
+ch++;
+}
+putchar ('\n');
+return (0);
+}
