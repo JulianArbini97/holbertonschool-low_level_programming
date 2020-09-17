ffbeecbe067bf08d0236b94b40ea088958c0d632
Julian <1890@holbertonschool.com>
2020-09-17 15:00:27 +0000

    0x01C

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -3,14 +3,18 @@
 /**
  * main - entry point
  *
+ * Description: Prints the alphabet with only 2 putchar and without q and e
+ *
  * Return: always 0 (Success)
  */
 int main(void)
 {
 int ch = 'a';
-
+int hc = 'A';
 while (ch <= 'z')
 putchar(ch++);
+while (hc <= 'Z')
+putchar(hc++);
 putchar('\n');
 return (0);
 }
