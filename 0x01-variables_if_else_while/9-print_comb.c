058f749caf32d1296f3ff4721f412b00d99e2a9e
Julian <1890@holbertonschool.com>
2020-09-17 18:39:33 +0000

    0x01C

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Return: always 0 (Success)
+ */
+int main(void)
+{
+int num = '0';
+while (num <= '9')
+{
+putchar (num);
+if (num != 9)
+{
+putchar(',');
+putchar(' ');
+}
+num++;
+}
+putchar ('\n');
+return (0);
+}
