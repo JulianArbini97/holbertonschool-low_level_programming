ca845f7e54a86a4cd980a23b1feb56f16496afeb
Julian <1890@holbertonschool.com>
2020-09-21 17:34:02 +0000

    0x02C

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "holberton.h"
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
+
+void print_alphabet(void)
+{
+char ch = 'a';
+while (ch <= 'z')
+_putchar(ch++);
+_putchar('\n');
+}
