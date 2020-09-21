d12a3915c04f02c227c496aa89675a114869b1cd
Julian <1890@holbertonschool.com>
2020-09-21 21:53:07 +0000

    0x02C

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,3 +1,4 @@
+#include "holberton.h"
 /**
  * main - check the code for Holberton School students.
  *
@@ -5,16 +6,29 @@
  */
 void jack_bauer(void)
 {
-  int a;
-  int b;
-  
-  for(a=0, a <= 24, a++)
-    {
-      putchar (a);
-    }
+int h1, h2, m1, m2;
 
-  for(b=0, b <= 60, b++)
-    { 
-      putchar (b);
-    }
+for(h1 = 0; h1 <= 2; h1++)
+{
+for(h2 = 0; h2 <= 9; h2++)
+{
+if (h1 == 2 && h2 == 4)
+{
+break;
+}
+for(m1 = 0; m1 <= 5; m1++)
+{
+for(m2 = 0; m2 <= 9; m2++)
+{
+_putchar(h1 + '0');
+_putchar(h2 + '0');
+_putchar(58);
+_putchar(m1 + '0');
+_putchar(m2 + '0');
+_putchar('\n');
+}		
+}
+}
+}
+}
  
