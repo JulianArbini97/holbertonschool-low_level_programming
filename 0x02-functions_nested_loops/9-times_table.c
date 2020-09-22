a65913213c182aaa3ecdf7d50c733858620c63bf
Julian <1890@holbertonschool.com>
2020-09-22 23:20:12 +0000

    0x02C

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -5,7 +5,31 @@
  *
  * Return: Always 0.
  */
-void times_table(void);
+void times_table(void)
 {
-  int a;
-  int b;
+int a, b, res, sig;
+
+for (a = 48; a <= 57; a++)
+{
+for (b = 48; b <= 57; b++)
+{
+res = (a - 48) * (b - 48);
+sig = (a - 48) * ((b + 1) - 48);
+if (res > 9)
+{
+_putchar((res / 10) + 48);
+_putchar((res % 10) + 48);
+}
+else
+_putchar(res + 48);
+if (b != 57)
+{
+_putchar(',');
+if (res <= 9 && sig <= 9 && b <= 57)
+_putchar(' ');
+_putchar(' ');
+}
+}
+_putchar(10);
+}
+}
