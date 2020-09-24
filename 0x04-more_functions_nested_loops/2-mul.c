1150e4a6b756e52f93d48d35908cfdc18a83bac5
Julian <1890@holbertonschool.com>
2020-09-24 17:19:21 +0000

    0x04C

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -3,10 +3,11 @@
 
 /**
  * mul - check the code for Holberton School students.
- *
+ * @a: var
+ * @b: var
  * Return: Always 0.
  */
 int mul(int a, int b)
 {
-  return (a * b);
+return (a * b);
 }
