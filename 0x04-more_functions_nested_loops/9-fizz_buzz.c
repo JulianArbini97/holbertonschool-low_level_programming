a090207a7205d251f766d9f63928039fac830c36
Julian <1890@holbertonschool.com>
2020-09-24 17:34:35 +0000

    0x04C

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -9,17 +9,17 @@ int main(void)
 int a;
 
 for (a = 1; a < 100; a++)
-if (a % 3 == 0)
+if (a % 15 == 0)
 {
-printf("Fizz ");
+printf("FizzBuzz ");
 }
 else if (a % 5 == 0)
 {
 printf("Buzz ");
 }
-else if (a % 15 == 0)
+ else if (a % 3 == 0)
 {
-printf("FizzBuzz ");
+printf("Fizz ");
 }
 else
 {
