f17575e3c91424afa47d8738ffccf3ffb6469c77
Julian <1890@holbertonschool.com>
2020-09-29 19:06:35 +0000

    0x05C

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -9,10 +9,7 @@
  */
 void swap_int(int *a, int *b)
 {
-int q = *a; /*Defino las variables con sus respectivos punteros */
-int w = *b;
-q = 98; /* Se le da el valor a la variable */
-w = 42;
-*a = w; /* Intercambio el valor de las variables con nuevos punteros */
-*b = q;
+int i = *a; /*Defino la variable con sus respectivo puntero */
+*a = *b;
+*b = n;
 }
