fd2ee75c2ddbc1e848c7d2394e9fbfd4a1196a63
Julian <1890@holbertonschool.com>
2020-09-29 19:53:15 +0000

    0x05C

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -1,27 +1,24 @@
 #include "holberton.h"
 #include <string.h>
 /**
- * main - check the code for Holberton School students.
- *
+ * puts_half - check the code for Holberton School students.
+ * @str: Puntero
  * Return: Always 0.
  */
 void puts_half(char *str)
 {
-  int x, y;
-  x = 0;
-  y = 0;
-
-  while (str[x] != '\0') /* Con esto va a ir avanzando en el string. El str[x] quiere decir que el string mide x (length) */
-    x++;
-
-  if (x % 2 == 0)
-    y = x / 2;
-  else
-    y = (x + 1) / 2;
-
-  for (; y < x; y++)
-    {
-      _putchar(str[y]);
-    }
-  _putchar('\n');
+int x, y;
+x = 0;
+y = 0;
+while (str[x] != '\0')
+x++;
+if (x % 2 == 0)
+y = x / 2;
+else
+y = (x + 1) / 2;
+for (; y < x; y++)
+{
+_putchar(str[y]);
+}
+_putchar('\n');
 }
