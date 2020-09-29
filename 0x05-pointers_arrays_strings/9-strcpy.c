2c7fe5cb46551af02048d593cf3f0d67438e6316
Julian <1890@holbertonschool.com>
2020-09-29 20:01:48 +0000

    0x05C

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -2,21 +2,21 @@
 #include <stdio.h>
 
 /**
- * main - check the code for Holberton School students.
- *
+ * _strcpy - check the code for Holberton School students.
+ * @dest: char pointer to be determined.
+ * @src: char pointer to be determined.
  * Return: Always 0.
  */
 char *_strcpy(char *dest, char *src)
 {
-  int a = 0;
-  int b;
+int a = 0;
+int b;
 
-  while (src[a] != '\0')
-    a++;
-
-  for (b = 0; b <= a; b++)
-    {
-      dest[b] = src[b];
-    }
-  return (dest);
+while (src[a] != '\0')
+a++;
+for (b = 0; b <= a; b++)
+{
+dest[b] = src[b];
+}
+return (dest);
 }
