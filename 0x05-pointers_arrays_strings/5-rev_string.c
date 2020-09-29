ea5198809725ae9c5a0539fd0d0c1e96371afb44
Julian <1890@holbertonschool.com>
2020-09-29 19:23:56 +0000

    0x05C

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,22 +1,22 @@
-#include "holberton.h"
+B#include "holberton.h"
 #include <string.h>
 
 /**
- * main - check the code for Holberton School students.
- *
+ * rev_string - check the code for Holberton School students.
+ * @s: Puntero
  * Return: Always 0.
  */
 void rev_string(char *s)
 {
-  int x = strlen(s) - 1;
-  int y = 0;
-  char z;
-  while (y < x)
-    {
-      z = s[y];
-      s[y] = s[x];
-      s[x] = z;
-      y++;
-      x--;
-    }
+int x = strlen(s) - 1;
+int y = 0;
+char z;
+while (y < x)
+{
+z = s[y];
+s[y] = s[x];
+s[x] = z;
+y++;
+x--;
+}
 }
