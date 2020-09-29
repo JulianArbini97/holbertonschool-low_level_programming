1416334ca0f5ed8e3b7a45bdaad494cf4c784956
Julian <1890@holbertonschool.com>
2020-09-29 19:14:46 +0000

    0x05C

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,18 +1,17 @@
 #include "holberton.h"
 #include <stdio.h>
 /**
- * main - check the code for Holberton School students.
- *
+ * _puts - check the code for Holberton School students.
+ * @str: Puntero
  * Return: Always 0.
  */
 void _puts(char *str)
 {
-  int i = 0;
-
-  while (*(str + i) != 0)
-    {
-      _putchar(*(str + i));
-      i++;
-    }
-  _putchar(10);
+int i = 0;
+while (*(str + i) != 0)
+{
+_putchar(*(str + i));
+i++;
+}
+_putchar(10);
 }
