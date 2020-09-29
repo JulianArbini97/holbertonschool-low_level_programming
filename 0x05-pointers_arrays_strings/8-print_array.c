77edff5ffd0db03684df2ad076390249e7c2fb4e
Julian <1890@holbertonschool.com>
2020-09-29 14:43:26 +0000

    0x05C

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -1,5 +1,5 @@
 #include "holberton.h"
-
+#include <stdio.h>
 /**
  * main - check the code for Holberton School students.
  *
@@ -7,3 +7,14 @@
  */
 void print_array(int *a, int n)
 {
+  int i = 0;
+  for (i=0; i<n; i++) 
+    { 
+      printf("%d", a[i]);
+      if (i < n - 1)
+	{
+	  printf(", ");
+	}
+    }
+  printf("\n");
+}
