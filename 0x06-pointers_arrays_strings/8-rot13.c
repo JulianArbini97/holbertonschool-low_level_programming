5ca3d877fdf6521fa1659b4ffc8f530d8f1c505b
Julian <1890@holbertonschool.com>
2020-10-01 18:03:56 +0000

    0x06C

---
+++ 0x06-pointers_arrays_strings/8-rot13.c
@@ -0,0 +1,27 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * rot13 - check the code for Holberton School students.
+ * @a: var
+ * Return: Always 0.
+ */
+char *rot13(char *a)
+{
+int i = 0, j = 0;
+char letra1[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+char letra2[] = "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM";
+while (*(a + i) != 0)
+{
+for (j = 0; j <= 52; j++)
+{
+if (*(a + i) == letra1[j])
+{
+*(a + i) = letra2[j];
+break;
+}
+}
+i++;
+}
+return (a);
+}
