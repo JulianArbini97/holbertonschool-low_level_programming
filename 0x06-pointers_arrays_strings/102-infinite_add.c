558ba8316527bedae760180729cb2a6669a5dfd9
Julian <1890@holbertonschool.com>
2020-10-02 11:59:37 +0000

    0x06C

---
+++ 0x06-pointers_arrays_strings/102-infinite_add.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * infinite_add - check the code for Holberton School students.
+ * @n1: punt
+ * @n2: punt
+ * @r: punt
+ * @size_r: var
+ * Return: Always 0.
+ */
+char *infinite_add(char *n1, char *n2, char *r, int size_r)
+{
---
+++ 0x06-pointers_arrays_strings/103-print_buffer.c
@@ -0,0 +1,11 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * print_buffer - check the code for Holberton School students.
+ * @b: punt
+ * @size: var
+ * Return: Always 0.
+ */
+void print_buffer(char *b, int size)
+{
