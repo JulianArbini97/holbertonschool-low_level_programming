0d567b0a31a0e407e59f220b7d363943479a54e9
JulianArbini97 <1890@holbertonschool.com>
2020-10-14 22:36:17 +0000

    Malloc

--- 0x0B-malloc_free/5-argstostr.c
+++ 0x0B-malloc_free/5-argstostr.c
@@ -4,32 +4,53 @@
 
 /**
  * main - check the code for Holberton School students.
- *
- * Return: Always 0.
+ * _strlen - returns the length of a string
+ * @s: string s
+ * Return: length of string
+ */
+int _strlen(char *s)
+{
+  int i = 0;
+
+  while (*s++)
+    i++;
+
+  return (i);
+}
+
+/**
+ * argstostr - concatenates all the arguments of your program
+ * @ac: argument count
+ * @av: argument vector
+ * Return: concatenated string
  */
 char *argstostr(int ac, char **av)
 {
-  char *p;
-  int i, j, total;
-
-  if (ac == 0 || av == NULL)
-    {
-      return (NULL);
-    }
-
-  p = malloc(sizeof(char) * ac);
-  for (i = 0; i < ac; i++)
-    {
-      for (j = 0; av[j] != 0; j++)
-	{
-	}
-	  total += j + 1;
-    }
-      p = malloc(sizeof(char) * total);
-      for (j = 0; av[j] != 0; j++)
-	{
-	  p += av[i][j];
-	  p += '\n';
-	}
-      return (p);
-    }
+int i, j, length, total;
+int m = 0;
+char *p;
+if (ac == 0 || av == NULL)
+{
+return (NULL);
+}
+total = 0;
+for (i = 0; i < ac; i++)
+{
+length = _strlen(av[i]) + 1;
+total += length;
+}
+p = malloc(sizeof(char) * total + 1);
+if (!p)
+return (NULL);
+for (i = 0; i < ac; i++)
+{
+length = _strlen(av[i]);
+for (j = 0; j < len; j++, m++)
+{
+p[m] = av[i][j];
+}
+p[m++] = '\n';
+}
+p[m] = '\0';
+return (p);
+}
