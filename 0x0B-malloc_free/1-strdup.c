2f45d8d21e2f11ca86c9850c43392f1dc15a787d
JulianArbini97 <1890@holbertonschool.com>
2020-10-14 15:47:06 +0000

    Malloc

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -1,32 +1,28 @@
-#include "holberton.h"
+B#include "holberton.h"
 #include <stdio.h>
 #include <stdlib.h>
 
 /**
- * main - check the code for Holberton School students.
- *
+ * _strdup - check the code for Holberton School students.
+ * @str: punt
  * Return: Always 0.
  */
 char *_strdup(char *str)
 {
-  int a = 0, b = 0;
-  char *s;
-
-  while (str[a] != 0)
-    {
-      a++;
-    }
-
-  if (a == 0)
-    {
-      return (NULL);
-    }
-
-  s = (char*)malloc(sizeof(char) * a);
-
-  for (b = 0; b < a; b++)
-    {
-      s[b] = str[b];
-    }
-  return (s);
+int a = 0, b = 0;
+char *s;
+while (str[a] != 0)
+{
+a++;
+}
+if (a == 0)
+{
+return (NULL);
+}
+s = (char *)malloc(sizeof(char) * a);
+for (b = 0; b < a; b++)
+{
+s[b] = str[b];
+}
+return (s);
 }
