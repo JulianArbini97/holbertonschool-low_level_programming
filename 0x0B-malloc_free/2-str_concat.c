0e11ca1e3d405638a5a8b1bacff3d00f769e5f83
JulianArbini97 <1890@holbertonschool.com>
2020-10-14 16:16:32 +0000

    Malloc

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -3,30 +3,39 @@
 #include <stdlib.h>
 
 /**
- * main - check the code for Holberton School students.
- *
+ * str_concat - check the code for Holberton School students.
+ * @s1: punt
+ * @s2: Punt
  * Return: Always 0.
  */
 char *str_concat(char *s1, char *s2)
 {
-  char *p;
-  int a = 0, b = 0, i = 0, j = 0;
-
-  while (s1[a] != 0)
-    a++;
-  while (s2[b] != 0)
-    b++;
-
-  p = (char*)malloc(sizeof(char) * (a + b));
-
-  for (i = 0; i < a; i++)
-    {
-      p[i] = s1[i];
-    }
-
-  for (j = i; j < b + i; j++)
-    {
-      p[j] = s2[j - i];
-    }
-  return (p);
+char *p;
+int a = 0, b = 0, i = 0, j = 0;
+if (s1 == 0)
+{
+return (NULL);
+}
+while (s1[a] != 0)
+a++;
+if (s2 == 0)
+{
+return (NULL);
+}
+while (s2[b] != 0)
+b++;
+p = (char *) malloc(sizeof(char) * (a + b + 2));
+if (p == NULL)
+{
+return (NULL);
+}
+for (i = 0; i < a; i++)
+{
+p[i] = s1[i];
+}
+for (j = i; j < b + i; j++)
+{
+p[j] = s2[j - i];
+}
+return (p);
 }
