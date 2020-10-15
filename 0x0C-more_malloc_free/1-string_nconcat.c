fdb84369ce84207741e5875c275cffaaeec79154
JulianArbini97 <1890@holbertonschool.com>
2020-10-15 16:01:37 +0000

    More Malloc

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,6 +1,7 @@
 #include "holberton.h"
 #include <stdlib.h>
 
+int _strlen(char *s);
 /**
  * string_nconcat - check the code for Holberton School students.
  * @s1: var
@@ -11,36 +12,51 @@
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 char *p;
-unsigned int a = 0, b = 0, i = 0, j = 0;
+unsigned int first = 0, second = 0, i = 0, j = 0;
 if (s1 == NULL)
 {
 s1 = "";
 }
-while (s1[a] != 0)
-a++;
 if (s2 == NULL)
 {
 s2 = "";
 }
-while (s2[b] != 0)
-b++;
-if (n >= b)
+first = _strlen(s1);
+second = _strlen(s2);
+if (n > second)
 {
-n = b;
+n = second;
 }
-p = (char *) malloc(sizeof(char) * (a + n)+1);
+p = malloc(sizeof(char) * (first + n)+1);
 if (p == NULL)
 {
 return (NULL);
 }
-for (i = 0; i < a; i++)
+for (i = 0; i < first; i++)
 {
 p[i] = s1[i];
 }
-for (j = i; j < n + i; j++)
+for (j = 0; j < n; j++)
 {
-p[j] = s2[j - i];
+p[i + j] = s2[j];
 }
-p[i + j + 1] = '\0';
+p[i + j] = '\0';
 return (p);
+}
+/**
+ * _strlen - check the code for Holberton School students.
+ * @s: var
+ * Return: Always 0.
+ */
+
+int _strlen(char *s)
+{
+int b = 0;
+int total = 0;
+while (*(s + b) != '\0')
+{
+b++;
+total++;
+}
+return (total);
 }
