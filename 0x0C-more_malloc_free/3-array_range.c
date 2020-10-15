500f1cd9bde4016b69cdfc32eb3934e8e122d63a
JulianArbini97 <1890@holbertonschool.com>
2020-10-15 17:01:02 +0000

    More Malloc

---
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -0,0 +1,27 @@
+#include "holberton.h"
+#include <stdlib.h>
+
+/**
+ * array_range - prints buffer in hexa
+ * @min: the address of memory to print
+ * @max: the size of the memory to print
+ *
+ * Return: Nothing.
+ */
+int *array_range(int min, int max)
+{
+int *p;
+int a;
+if (min > max)
+{
+return(0);
+}
+p = malloc (sizeof(int) * (max - min + 1));
+if (!p)
+return (0);
+for (a = 0; min <= max; min++, a++)
+{
+p[a] = min;
+}
+return (p);
+}
