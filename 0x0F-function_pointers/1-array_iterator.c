590322301c7329b4074f691abf230aecf9570a00
JulianArbini97 <1890@holbertonschool.com>
2020-10-21 15:46:39 +0000

    0x0F

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "function_pointers.h"
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+  unsigned int i = 0;
+  if (size <= 0 || array == NULL || action == NULL)
+    {
+      return;
+    }
+  
+for ( i = 0; i <size; i++)
+    {
+      action(array[i]);
+    }
+}
