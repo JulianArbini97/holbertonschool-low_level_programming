16f5358a0b960c09a3d9ea7a0701edd9ff7f7952
JulianArbini97 <1890@holbertonschool.com>
2020-10-21 19:14:10 +0000

    0x0F

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,9 +1,14 @@
 #include <stdio.h>
 #include "function_pointers.h"
 
+/**
+ * print_name - check the code for Holberton School students.
+ * @name: variable that is going to receive the value of another function.
+ * @f: pointer to function.
+ */
 
 void print_name(char *name, void (*f)(char *))
 {
-  if (name != 0 && f != 0)
-  f(name);
+if (name != 0 && f != 0)
+f(name);
 }
--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -1,16 +1,22 @@
 #include <stdio.h>
 #include "function_pointers.h"
 
+/**
+ * array_iterator - check the code for Holberton School students.
+ * @array: Array that is going to receive all the values.
+ * @size: Size.
+ * @action: Pointer to another function
+ */
+
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
-  unsigned int i = 0;
-  if (size <= 0 || array == NULL || action == NULL)
-    {
-      return;
-    }
-  
-for ( i = 0; i <size; i++)
-    {
-      action(array[i]);
-    }
+unsigned int i = 0;
+if (size <= 0 || array == NULL || action == NULL)
+{
+return;
+}
+for (i = 0; i < size; i++)
+{
+action(array[i]);
+}
 }
