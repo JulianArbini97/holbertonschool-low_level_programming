1ccd50085a29cbf9e23d15908def1b1dd7d84712
JulianArbini97 <1890@holbertonschool.com>
2020-10-21 19:24:10 +0000

    0x0F

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,14 +1,16 @@
 #include <stdio.h>
 #include "function_pointers.h"
-
 /**
- * print_name - check the code for Holberton School students.
- * @name: variable that is going to receive the value of another function.
- * @f: pointer to function.
+ *print_name - prints a name
+ *@name: name
+ *@f: function pointer
+ *Return - nothing
  */
-
 void print_name(char *name, void (*f)(char *))
 {
-if (name != 0 && f != 0)
+if (f == NULL || name == NULL)
+{
+return;
+}
 f(name);
 }
--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,3 +1,10 @@
-int _putchar(char c);
-void print_name(char *name, void (*f)(char *));
+#ifndef FUNCTION_POINTERS
+#define FUNCTION_POINTERS
 void array_iterator(int *array, size_t size, void (*action)(int));
+void print_name(char *name, void (*f)(char *));
+void print_name_as_is(char *name);
+void print_name_uppercase(char *name);
+int _putchar(char c);
+int int_index(int *array, int size, int (*cmp)(int));
+
+#endif
