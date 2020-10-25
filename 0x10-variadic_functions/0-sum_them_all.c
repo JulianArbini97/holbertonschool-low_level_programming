630bdef1874506351991611b8a4cd09e6b8102fa
JulianArbini97 <1890@holbertonschool.com>
2020-10-25 21:58:19 +0000

    0x10C

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -12,11 +12,13 @@ int sum_them_all(const unsigned int n, ...)
 va_list lista;
 unsigned int i;
 int sum = 0;
+
+va_start(lista, n);
 if (n == 0)
 {
 return (0);
 }
-va_start(lista, n);
+
 for (i = 0; i < n; i++)
 {
 sum += va_arg(lista, int);
