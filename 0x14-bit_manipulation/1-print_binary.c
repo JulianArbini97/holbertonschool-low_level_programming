f4700460779a716e38c6ddac5eea93f270c70684
JulianArbini97 <1890@holbertonschool.com>
2020-11-05 20:06:00 +0000

    0x14

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * print_binary - print a number in binary
+ * @n: the number
+ * Return: Nothing
+ */
+
+void print_binary(unsigned long int n)
+{
+if (n == 0)
+{
+_putchar('0');
+}
+else if (n == 1)
+_putchar('1');
+else
+{
+print_binary(n >> 1);
+_putchar((n & 1) + '0');
+}
+}
