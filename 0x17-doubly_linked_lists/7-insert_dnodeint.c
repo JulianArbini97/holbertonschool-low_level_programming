d7ec91ac6f932810a1c0b6171a8bb122fd6340b4
JulianArbini97 <1890@holbertonschool.com>
2020-12-14 17:26:22 +0000

    0x17

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -1,53 +1,43 @@
 #include <stdlib.h>
 #include <stdio.h>
-#include <string.h>
 #include "lists.h"
-
 /**
- * insert_dnodeint_at_index - adds a node at the beginning of a dlistint_t list
- * @h: input header pointer
- * @idx:  index of the list where the new node should be added
- * @n: value of the node
- * Return: address of the new element, or NULL if it failed
+ * insert_dnodeint_at_index - inserts a new node at given position
+ * @h: head of the node
+ * @idx: index where to place node
+ * @n: data for the node
+ * Return: address of the new node, or NULL if it failed
  */
-
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-	dlistint_t *temporal = *h, *tempcount = *h, *temporal2;
+	dlistint_t *tmp = *h, *tmpcount = *h, *tmp2;
 	dlistint_t *new_node;
-	unsigned int counter = 0;
+	unsigned int cont = 0;
 
 	if (*h == NULL)
 		return (NULL);
-	while (tempcount != NULL)
-		tempcount = tempcount->next, counter++;
-	if (idx > counter)
+	while (tmpcount != NULL)
+		tmpcount = tmpcount->next, cont++;
+	if (idx > cont)
 		return (NULL);
 	new_node = malloc(sizeof(dlistint_t));
 	if (new_node == NULL)
 		return (NULL);
 	new_node->n = n;
-	new_node->prev = NULL;
-	new_node->next = NULL;
 	if (idx == 0)
 	{
 		new_node->prev = NULL, new_node->next = *h;
 		if (*h)
-		{
-
 			(*h)->prev = new_node;
-		}
 		*h = new_node;
 		return (new_node);
 	}
-	for (counter = 0; counter < idx; counter++)
-		temporal2 = temporal, temporal = temporal->next;
-	new_node->next = temporal;
-	new_node->prev = temporal2;
-	temporal2->next = new_node;
-	if (temporal)
-	{
-		temporal->prev = new_node;
-	}
+	for (cont = 0; cont < idx; cont++)
+		tmp2 = tmp, tmp = tmp->next;
+	new_node->next = tmp;
+	new_node->prev = tmp2;
+	tmp2->next = new_node;
+	if (tmp)
+		tmp->prev = new_node;
 	return (new_node);
 }
