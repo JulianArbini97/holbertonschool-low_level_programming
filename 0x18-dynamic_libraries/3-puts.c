427b023a5354c102d113ebab05720edb456331e6
JulianArbini97 <1890@holbertonschool.com>
2020-12-15 17:59:39 +0000

    0x17

---
+++ 0x18-dynamic_libraries/3-puts.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "holberton.h"
+/**
+ *_puts - print a string
+ *@str: pointer to str
+ *
+ **/
+void _puts(char *str)
+{
+int i;
+for (i = 0; str[i] != '\0'; i++)
+{
+_putchar(str[i]);
+}
+_putchar('\n');
+}
