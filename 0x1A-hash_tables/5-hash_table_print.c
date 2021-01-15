bbbe193950482993a727ad772d6793a9cfd50c8b
JulianArbini97 <1890@holbertonschool.com>
2021-01-15 13:39:02 +0000

    task 5

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -5,32 +5,26 @@
  * @ht: is the hash table you want to add or update the key/value to
  * Return: 1 if it succeeded, 0 otherwise
  */
+
 void hash_table_print(const hash_table_t *ht)
 {
 unsigned int i;
-unsigned int j = 0;
+hash_node_t *tmp = NULL;
+char *c = "";
 
 if (ht == NULL)
-{
 return;
-}
-while (ht->array[j] == NULL)
-{
-j++;
-}
-
 printf("{");
 for (i = 0; i < ht->size; i++)
 {
 if (ht->array[i] != NULL)
 {
-if (i == j)
+tmp = ht->array[i];
+while (tmp != NULL)
 {
-printf("'%s': '%s'", ht->array[i]->key, ht->array[i]->value);
-}
-else
-{
-printf(", '%s': '%s'", ht->array[i]->key, ht->array[i]->value);
+printf("%s'%s': '%s'", c, tmp->key, tmp->value);
+c = ", ";
+tmp = tmp->next;
 }
 }
 }
