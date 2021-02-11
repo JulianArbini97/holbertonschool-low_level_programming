18b7c897a63addee982893dd90c2683676766a2b
JulianArbini97 <1890@holbertonschool.com>
2021-02-11 19:19:21 +0000

    5

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -2,25 +2,29 @@
 """ Perimeter function"""
 
 def island_perimeter(grid):
-    """ island perimeter """
-    perimetro = 0;
-    for fila in range (len(grid)):
-        for celda in range (len(grid[0])):
+    """ def """
+    per = 0
+
+    for fila in range(len(grid)):
+        for celda in range(len(grid[0])):
             if grid[fila][celda] == 1:
-                if (fila <= 0):
-                    perimetro += 1
-                elif grid[fila-1][celda] == 0:
-                    perimetro += 1
-                if (fila >= len(grid[0]) - 1):
-                    perimetro += 1
-                elif grid[fila+1][celda] == 0:
-                    perimetro += 1
-                if (celda <= 0):
-                    perimetro += 1
-                elif grid[fila][celda-1] == 0:
-                    perimetro += 1
-                if (celda >= len(grid) - 1):
-                    perimetro += 1
-                elif grid[fila][celda+1] == 0:
-                    perimetro += 1
-    return perimetro
+                if fila <= 0:
+                    per = per + 1
+                elif grid[fila - 1][celda] != 1:
+                    per = per + 1
+
+                if celda <= 0:
+                    per = per + 1
+                elif grid[fila][celda - 1] != 1:
+                    per = per + 1
+
+                if fila >= (len(grid) - 1):
+                    per = per + 1
+                elif grid[fila + 1][celda] != 1:
+                    per = per + 1
+
+                if celda >= (len(grid[0]) - 1):
+                    per = per + 1
+                elif grid[fila][celda + 1] != 1:
+                    per = per + 1
+    return per
