be2fd5a6fd5328fd73826885cfc5499748519bb2
JulianArbini97 <1890@holbertonschool.com>
2021-02-11 19:16:26 +0000

    5

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -4,9 +4,23 @@
 def island_perimeter(grid):
     """ island perimeter """
     perimetro = 0;
-    for fila in grid:
-        for celda in fila:
-            if fila[celda] == 1:
-                perimetro = perimetro + 2
-    perimetro = perimetro + 2
+    for fila in range (len(grid)):
+        for celda in range (len(grid[0])):
+            if grid[fila][celda] == 1:
+                if (fila <= 0):
+                    perimetro += 1
+                elif grid[fila-1][celda] == 0:
+                    perimetro += 1
+                if (fila >= len(grid[0]) - 1):
+                    perimetro += 1
+                elif grid[fila+1][celda] == 0:
+                    perimetro += 1
+                if (celda <= 0):
+                    perimetro += 1
+                elif grid[fila][celda-1] == 0:
+                    perimetro += 1
+                if (celda >= len(grid) - 1):
+                    perimetro += 1
+                elif grid[fila][celda+1] == 0:
+                    perimetro += 1
     return perimetro
