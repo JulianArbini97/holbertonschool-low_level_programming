bbb2d9c80605044d2887b8b3ee6bdf32b0bd65c9
JulianArbini97 <1890@holbertonschool.com>
2021-04-20 18:52:50 +0000

    100 101

--- 0x1E-search_algorithms/100-jump.c
+++ 0x1E-search_algorithms/100-jump.c
@@ -5,40 +5,40 @@
 #include <math.h>
 
 /**
- * linear_search to get THE number
- * @array: Array to work with
- * @size: Size of the array
- * @value: Value to search
- * Return: 0 or -1
- */
+* jump_search - get THE number
+* @array: Array to work with
+* @size: Size of the array
+* @value: Value to search
+* Return: 0 or -1
+*/
 
 int jump_search(int *array, size_t size, int value)
 {
-	size_t jump_step, prev = 0;
+size_t jump_step, prev = 0;
 
-    if (array == NULL || size == 0)
-		return -1;
+if (array == NULL || size == 0)
+return (-1);
 
-   jump_step = sqrt(size);
-	while (array[prev] <= value)
-	{
-		printf("Value checked array[%li] = [%i]\n", prev, array[prev]);
-		if (array[jump_step] >= value || jump_step >= size)
-		{
-			break;
-		}
-        prev = jump_step;
-        jump_step = jump_step + sqrt(size);
-	}
-    printf("Value found between indexes [%li] and [%li]\n", prev, jump_step);
-	while(array[prev] <= value && prev < size)
-	{
-		printf("Value checked array[%li] = [%i]\n", prev, array[prev]);
-        if(array[prev] == value)
-	    {
-		return prev;
-	    }
-        prev++;
-	}
-    return -1;
+jump_step = sqrt(size);
+while (array[prev] <= value)
+{
+printf("Value checked array[%li] = [%i]\n", prev, array[prev]);
+if (array[jump_step] >= value || jump_step >= size)
+{
+break;
+}
+prev = jump_step;
+jump_step = jump_step + sqrt(size);
+}
+printf("Value found between indexes [%li] and [%li]\n", prev, jump_step);
+while (array[prev] <= value && prev < size)
+{
+printf("Value checked array[%li] = [%i]\n", prev, array[prev]);
+if (array[prev] == value)
+{
+return (prev);
+}
+prev++;
+}
+return (-1);
 }
