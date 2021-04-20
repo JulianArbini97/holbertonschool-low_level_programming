5c0240643281b310ca36a23616b2d19afd7e6ed5
JulianArbini97 <1890@holbertonschool.com>
2021-04-20 15:58:50 +0000

    1 fix betty

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -3,7 +3,7 @@
 #include <unistd.h>
 #include <stdlib.h>
 /**
- * linear_search to get THE number
+ * binary_search - get THE number
  * @array: Array to work with
  * @size: Size of the array
  * @value: Value to search
@@ -12,44 +12,44 @@
 
 int binary_search(int *array, size_t size, int value)
 {
-    size_t left, right, middle, counter;
-    
-    if (array == NULL)
-    {
-        return (-1);
-    }
+size_t left, right, middle, counter;
 
-    left = 0;
-    right = size - 1;
+if (array == NULL)
+{
+return (-1);
+}
 
-    while (left <= right)
-    {
-        printf("Searching in array: ");
-        for (counter = left; counter <= right; counter++)
-        {
-            printf("%i", array[counter]);
-            if (counter < right)
-            {
-                printf(", ");
-            }
-            else
-            {
-                printf("\n");
-            }
-        }
-        middle = (left + right)/2;
-        if (array[middle] == value)
-        {
-            return (middle);
-        }
-        if (array[middle] < value)
-        {
-            left = middle + 1;
-        }
-        if (array[middle] > value)
-        {
-            right = middle - 1;
-        }
-    }
-    return (-1);
+left = 0;
+right = size - 1;
+
+while (left <= right)
+{
+printf("Searching in array: ");
+for (counter = left; counter <= right; counter++)
+{
+printf("%i", array[counter]);
+if (counter < right)
+{
+printf(", ");
+}
+else
+{
+printf("\n");
+}
+}
+middle = (left + right) / 2;
+if (array[middle] == value)
+{
+return (middle);
+}
+if (array[middle] < value)
+{
+left = middle + 1;
+}
+if (array[middle] > value)
+{
+right = middle - 1;
+}
+}
+return (-1);
 }
