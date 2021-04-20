fd394ca12fd348163131c54205075267fc854b23
JulianArbini97 <1890@holbertonschool.com>
2021-04-20 15:54:38 +0000

    0 fixes

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -2,8 +2,9 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <stdlib.h>
+
 /**
- * linear_search to get THE number
+ * linear_search - get THE number
  * @array: Array to work with
  * @size: Size of the array
  * @value: Value to search
@@ -12,29 +13,18 @@
 
 int linear_search(int *array, size_t size, int value)
 {
-    int i = 0;
+size_t i = 0;
 
-    if (array == NULL)
-    {
-        return (-1);
-    }
+if (array == NULL)
+{
+return (-1);
+}
 
-    for (i = 0; i < (int)size; i++)
-    {
-        if (array[i] == value)
-        {
-            printf("Value checked array[%d] = [%d]\n", i, array[i]);
-            break;
-        }
-        else
-        {
-            printf("Value checked array[%d] = [%d]\n", i, array[i]);
-        }
-    }
-
-    if (i > (int)size)
-    {
-        return (-1);
-    }
-    return(i);
+for (i = 0; i < size; i++)
+{
+printf("Value checked array[%li] = [%d]\n", i, array[i]);
+if (array[i] == value)
+return (i);
+}
+return (-1);
 }
